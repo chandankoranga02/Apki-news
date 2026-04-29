ed80c49d33d0f927385adba9d2f8da5de110c33f
Chandan koranga <chandan.koranga.2025@outlook.com>
2026-04-29 19:14:34 +0530

    All warning error fixed

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
-    
-    constructor(){
-      super();
-    }
+
 
 
     render() {
@@ -12,11 +9,11 @@ export default class NewsItem extends Component {
         return (
 
             <div className="card" style={mode === "light" ? {border : "none"} : {border : "none"}}>
-                <img className="card-img-top" src={urlToImage} alt="Card image cap" />
+                <img className="card-img-top" src={urlToImage} alt="Card cap" />
                 <div className="card-body"  style={ mode === "light" ? {color : "black", backgroundColor: "white"} : {color : "white", backgroundColor: "black"}}>
                     <h5 className="card-title">{title}...</h5>
                     <p className="card-text">{description}...</p>
-                    <a href={url} target='_blank' className="btn btn-primary">Read More</a>
+                    <a href={url} target='_blank' rel="noopener noreferrer" className="btn btn-primary">Read More</a>
                 </div>
             </div>
 
